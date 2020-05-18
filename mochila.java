d7f5ad14bfae737b309f77991f9fc436ca9950c9
Gustavo Jacob <gustavo_jacob@localhost.localdomain>
2020-05-17 21:14:40 -0300

    update

--- mochila.java
+++ mochila.java
@@ -1,38 +1,49 @@
 //Problema da mochila resolvido com uso de recursão
-//Código baseado em um outro programa encontrado na internet
-
-//falta implementar os metodos para coletar as estatisticas
 
 class mochila {
+    public static int Iteracao = 0;
 
     public static void main(String args[]) {
-        int val[] = new int[] { 12, 2, 1, 1, 4 }; // valor
-        int wt[] = new int[] { 4, 2, 2, 1, 10 }; // peso
-        int W = 15; // capacidade da mochila
+        double inicio = System.nanoTime();
+
+        int val[] = new int[] { 92, 57, 49, 68, 60, 43, 67, 84, 87, 72 }; // valor
+        int wt[] = new int[] { 23, 31, 29, 44, 53, 38, 63, 85, 89, 82 }; // peso
+        int W = 165; // capacidade da mochila
         int n = val.length;
         System.out.println(knapSack(W, wt, val, n));
+
+        double fim = System.nanoTime();
+
+        double tempoTotal = fim - inicio;
+        System.err.println("Tempo total: " + tempoTotal);
+        System.out.println("Numero de interações: " + Iteracao);
+
     }
 
     static int knapSack(int W, int wt[], int val[], int n) {
-        // Base Case
+        
         if (n == 0 || W == 0)
+            addIteracao();
             return 0;
 
-        // If weight of the nth item is more
-        // than Knapsack capacity W, then
-        // this item cannot be included in the optimal solution
+      
         if (wt[n - 1] > W)
+            addIteracao();
             return knapSack(W, wt, val, n - 1);
 
-        // Return the maximum of two cases:
-        // (1) nth item included
-        // (2) not included
+        // retorna o valor maximo
         else
+            addIteracao();
             return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt, val, n - 1));
     }
 
     // Compara e retorna o maior
     static int max(int a, int b) {
+        addIteracao();
         return (a > b) ? a : b;
+    }
+
+    public static void addIteracao() {
+        Iteracao++;
     }
 }
